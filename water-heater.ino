333f0fa64a283ed8d48dab69688c98b0ac539d4c
Assaf Inbal <shmuelzon@gmail.com>
2016-01-03 10:01:38 +0200

    Added support for WS2812B LEDs

--- water-heater.ino
+++ water-heater.ino
@@ -1,5 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <ArduinoOTA.h>
+#include <Adafruit_NeoPixel.h>
 #include <InputDebounce.h>
 
 const char *ssid PROGMEM = ".......";
@@ -7,8 +8,12 @@ const char *password PROGMEM = ".......";
 const char *host PROGMEM = "water-heater";
 
 #define RELAY_PIN 4
+#define NEOPIXEL_PIN 5
 #define BUTTON_PIN 14
 
+#define NUM_PIXELS 4
+
+static Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 static InputDebounce button;
 
 static bool getRelayState(void) {
@@ -16,7 +21,12 @@ static bool getRelayState(void) {
 }
 
 static bool setRelayState(bool on) {
+  uint32_t color = on ? 0xFF0000 : 0x000000;
+
   digitalWrite(RELAY_PIN, on ? HIGH : LOW);
+  for (int i = 0; i < NUM_PIXELS; i++)
+    pixels.setPixelColor(i, color);
+  pixels.show();
 }
 
 static void toggleRelay(void) {
@@ -44,6 +54,11 @@ void setup() {
   /* Button */
   button.setup(BUTTON_PIN);
   button.registerCallbacks(toggleRelay, NULL, NULL);
+
+  /* NeoPixles */
+  pixels.begin();
+  pixels.setBrightness(16);
+  pixels.show();
 }
 
 void loop() {
