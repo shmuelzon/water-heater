9363259de6f79d13b3a22ef51df4756b68a09cb6
Assaf Inbal <shmuelzon@gmail.com>
2016-01-03 10:00:06 +0200

    Added button to toggle the relay

--- water-heater.ino
+++ water-heater.ino
@@ -1,11 +1,34 @@
 #include <ESP8266WiFi.h>
 #include <ArduinoOTA.h>
+#include <InputDebounce.h>
 
 const char *ssid PROGMEM = ".......";
 const char *password PROGMEM = ".......";
 const char *host PROGMEM = "water-heater";
 
+#define RELAY_PIN 4
+#define BUTTON_PIN 14
+
+static InputDebounce button;
+
+static bool getRelayState(void) {
+  return digitalRead(RELAY_PIN) == HIGH;
+}
+
+static bool setRelayState(bool on) {
+  digitalWrite(RELAY_PIN, on ? HIGH : LOW);
+}
+
+static void toggleRelay(void) {
+  setRelayState(!getRelayState());
+}
+
 void setup() {
+  /* GPIOs */
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
   /* WiFi */
   WiFi.mode(WIFI_STA);
   WiFi.hostname(host);
@@ -17,8 +40,13 @@ void setup() {
   /* OTA */
   ArduinoOTA.setHostname(host);
   ArduinoOTA.begin();
+
+  /* Button */
+  button.setup(BUTTON_PIN);
+  button.registerCallbacks(toggleRelay, NULL, NULL);
 }
 
 void loop() {
   ArduinoOTA.handle();
+  button.process(millis());
 }
